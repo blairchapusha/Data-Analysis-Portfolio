f4c213c2d80114bdfab33e0f8f4127fbc5c50a22
blairchapusha <101846771+blairchapusha@users.noreply.github.com>
2022-07-05 10:04:55 -0500

    Update project_analysis.sql

--- Project 1 - Credit Card Customers/project_analysis.sql
+++ Project 1 - Credit Card Customers/project_analysis.sql
@@ -1,13 +1,17 @@
 --Create database to store new data
+
 CREATE DATABASE credit_data;
 GO
 
 --Specify the database
+
 USE credit_data;
 GO
 
 --Create and format table to which the data will be inserted
 --Ensure the columns do not take null values so that no blank data observations are imported
+--Set client_id as the unique primary key
+
 CREATE TABLE customer_data (
 client_id INT NOT NULL PRIMARY KEY, customer_status VARCHAR(100) NOT NULL, age INT NOT NULL,
 gender VARCHAR(1) NOT NULL, dependents INT NOT NULL, education VARCHAR(100) NOT NULL,
@@ -17,43 +21,53 @@ revolving_balance DECIMAL(10,2) NOT NULL, transaction_amount DECIMAL(10,2) NOT N
 );
 GO
 
---Describe the table to validate details
-EXEC SP_COLUMNS customer_data;
-GO
+--Ensure table is empty using truncate before importing data
 
---Ensure table is empty using truncate
 TRUNCATE TABLE customer_data;
 GO
 
 
---Utilize bulk insert to import data from CSV dataset using path, after modifying CSV file to match customer_data table
+--Utilize bulk insert to import data from CSV dataset using specified path, after modifying CSV file to match customer_data table
+--Make the second row of the data in the CSV file as the first row of the table customer_data
 BULK INSERT customer_data
-FROM 'C:\Users\bchap\OneDrive\Documents\Data Analysis Projects\Project 1 - Credit Card Customers\Dataset\bank_churners_dataset.csv'
+FROM [FILE PATH]
 WITH (
 	FORMAT = 'CSV',
 	FIRSTROW = 2
 )
 GO
 
---Display table with data
+--Display table with data with further analyze
+
 SELECT *
 FROM customer_data;
 GO
 
 --Remove any rows that contain incomplete or unusable data
+
 DELETE FROM customer_data
 WHERE education = 'unknown'
 	OR marital_status = 'unknown' 
 	OR income = 'unknown';
 GO
 
+--Perform analysis to better understand the dataset
+
+SELECT count(*) FROM customer_data;
+GO
+SELECT DISTINCT age FROM customer_data;
+GO
+SELECT AVG(age) AS average_age FROM customer_data;
+
 --Create columns to specify ranges to the data
+
 ALTER TABLE customer_data
 ADD age_group VARCHAR(100), 
 	duration VARCHAR(100),
 	usage VARCHAR(100);
 
 --Create age ranges in increments of 11
+
 UPDATE customer_data
 SET age_group = '20-30'
 WHERE age BETWEEN 20 AND 30;
@@ -72,6 +86,7 @@ WHERE age BETWEEN 50 AND 75;
 GO
 
 --Update duration with two catergories
+
 UPDATE customer_data
 SET duration = 'Short-term Customer'
 WHERE months_active <= 24;
@@ -82,6 +97,7 @@ WHERE months_active > 24;
 GO
 
 --Set usage catergories
+
 UPDATE customer_data
 SET usage = 'Light Use'
 WHERE revolving_balance < 500;
